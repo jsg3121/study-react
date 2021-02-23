cdf0e05a87776a713d595eb1ce808529931e83de
jsg3121 <xodm95@gmail.com>
2021-02-23 16:09:58 +0900

    FEAT( Cut ) : blueprint closed

--- ui-framework/src/components/BluePrintComponents.tsx
+++ ui-framework/src/components/BluePrintComponents.tsx
@@ -1,59 +1,72 @@
-import React from 'react';
-// import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Cell, Column, Table } from '@blueprintjs/table';
 
-// const http = axios;
+const http = axios;
 
-// interface tableInterface {
-//   id: number;
-//   culture_name: string;
-//   category: string;
-//   district: string;
-//   start_date: string;
-//   end_date: string;
-//   usage_fee: string;
-// }
+interface tableInterface {
+  id: number;
+  culture_name: string;
+  category: string;
+  district: string;
+  start_date: string;
+  end_date: string;
+  usage_fee: string;
+}
 
 const BluePrintComponents = () => {
-  // const [Data, setData] = useState([{
-  //   key: 0,
-  //   title: "",
-  //   category: "",
-  //   district: "",
-  //   start_date: "",
-  //   end_date: "",
-  //   usage_fee: ""
-  // }]);
+  const [data, setData] = useState([{
+    key: 0,
+    title: "",
+    category: "",
+    district: "",
+    start_date: "",
+    end_date: "",
+    usage_fee: ""
+  }]);
+  const [loading, setLoading] = useState(true);
 
-  // http.get("/api/greeting").then((res) => {
-  //   const getData = res.data.map((list: tableInterface) => {
-  //     return {
-  //       key: list.id,
-  //       title: list.culture_name,
-  //       category: list.category,
-  //       district: list.district,
-  //       start_date: list.start_date,
-  //       end_date: list.end_date,
-  //       usage_fee: list.usage_fee,
-  //     };
-  //   });
-  //   setData(getData);
-  // });
-  console.log(1);
+  useEffect(() => {
+    http.get("/api/greeting").then((res) => {
+      const getData = res.data.map((list: tableInterface) => {
+        return {
+          key: list.id,
+          title: list.culture_name,
+          category: list.category,
+          district: list.district,
+          start_date: list.start_date,
+          end_date: list.end_date,
+          usage_fee: list.usage_fee,
+        };
+      });
+      setData(getData);
+      setLoading(false);
+    });
+  }, []);
 
   const cellRenderer = (index: number) => {
-    console.log(1231516151);
-    return <Cell>{index}</Cell>;
+    return (
+      <Cell>{data[index]}</Cell>
+    );
   };
-  console.log(2);
 
-  return (
-    <div>
-      <Table numRows={10}>
-        <Column name="dataCol" cellRenderer={cellRenderer} />
-      </Table>
-    </div>
-  );
+  if (loading) {
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Table numRows={10}>
+          <Column name="Title" cellRenderer={cellRenderer} />
+          <Column name="Category" cellRenderer={cellRenderer} />
+          <Column name="District" cellRenderer={cellRenderer} />
+        </Table>
+      </div>
+    );
+  }
 };
 
 export default BluePrintComponents;

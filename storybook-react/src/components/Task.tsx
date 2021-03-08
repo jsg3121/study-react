88726439460ca770d1ac5401ff5eae063e0890cf
jsg3121 <xodm95@gmail.com>
2021-03-08 11:24:49 +0900

    FEAT( stroybook ) : 스토리북 컴포넌츠 작성 (타입 미설정)

--- storybook-react/src/components/Task.tsx
+++ storybook-react/src/components/Task.tsx
@@ -7,14 +7,29 @@ export interface TaskProps {
     state?: string;
     updatedAt?: Date;
   },
-  onArchiveTask?: () => void,
-  onPintask?: () => void;
+  onArchiveTask: (id?: string) => void,
+  onPinTask: (id?: string) => void;
 }
 
-const Task: React.FC<TaskProps> = ({ task: { id, title, state, updatedAt }, onArchiveTask, onPintask }) => {
+const Task: React.FC<TaskProps> = ({ task: { id, title, state, updatedAt }, onArchiveTask, onPinTask }) => {
   return (
-    <div className="list-item">
-      <input type="text" value={title} readOnly={true} />
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input type="checkbox" name="checked" defaultChecked={state === 'TASK_ARCHIVED'} disabled={true} />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)}></span>
+      </label>
+      <div className="title">
+        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+      </div>
+
+      <div className="actions" onClick={event => event.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`} />
+          </a>
+        )}
+      </div>
     </div>
   );
 };
--- storybook-react/src/components/TaskList.tsx
+++ storybook-react/src/components/TaskList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import Task from './Task';
 
-import Task, { TaskProps } from './Task';
 
 interface tasksList {
   id?: string,
@@ -11,29 +11,56 @@ interface tasksList {
 
 export interface TaskListProps {
   loading?: boolean,
-  tasks?: Array<tasksList>,
+  tasks?: any,
   onPinTask?: () => void,
   onArchiveTask?: () => void;
 
 }
 
-const TaskList: React.FC<TaskListProps> = ({ loading, tasks, onPinTask, onArchiveTask }) => {
+const TaskList: React.FC<any> = ({ loading, tasks, onPinTask, onArchiveTask }) => {
   const events = {
     onPinTask,
-    onArchiveTask
+    onArchiveTask,
   };
 
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
   if (loading) {
-    return <div className="list-items">loading</div>;
+    return (
+      <div className="list-items">
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
   }
-
   if (tasks.length === 0) {
-    return <div className="list-items">empty</div>;
+    return (
+      <div className="list-items">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <div className="title-message">You have no tasks</div>
+          <div className="subtitle-message">Sit back and relax</div>
+        </div>
+      </div>
+    );
   }
-
+  const tasksInOrder = [
+    ...tasks.filter((t: any) => t.state === 'TASK_PINNED'),
+    ...tasks.filter((t: any) => t.state !== 'TASK_PINNED'),
+  ];
   return (
     <div className="list-items">
-      {tasks.map(task => (
+      {tasksInOrder.map(task => (
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
